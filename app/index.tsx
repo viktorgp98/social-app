cd3a3a89a0065ee07af00b43b6487d8cbb6469ce
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-27 22:37:22 -0400

    On reload app load the login view

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 const Index = () => {
-  return <Redirect href="/(auth)/login" />;
+  return <Redirect href="/(auth)/sign-in" />;
 };
 
 export default Index;
