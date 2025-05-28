f57b0ce443ca4e3383f718abc286fffc162e3aec
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-27 22:05:23 -0400

    Signup method update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,15 @@
-import InitialLayout from "@/components/initialLayout";
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { Slot } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
-
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
-if (!publishableKey) {
-  throw new Error(
-    "Missing Publishable Key. Please set the EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env file"
-  );
-}
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
-          <InitialLayout />
+          <Slot />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
