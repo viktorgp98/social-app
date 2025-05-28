f6c496b983529a4d4ed30d73277813c7c6fd1158
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-28 19:39:15 -0400

    Redirect depricated

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
-import { Redirect } from "expo-router";
+/* import { Redirect } from "expo-router";
 import React from "react";
 
 const Index = () => {
   return <Redirect href="/(auth)/sign-in" />;
 };
 
-export default Index;
+export default Index; */
