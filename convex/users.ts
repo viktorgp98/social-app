e06658091aac019547b64f392c97547742940037
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-28 19:41:05 -0400

    Create new user

---
+++ convex/users.ts
@@ -0,0 +1,36 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+// Create a new user
+export const createUser = mutation({
+  args:{
+    username:v.string(),
+    fullname:v.string(),
+    image:v.string(),
+    bio:v.optional(v.string()),
+    email:v.string(),
+    clerkId:v.string(),
+  },
+  handler: async (ctx,args)=>{
+    //if user exist verification
+    const existingUser= await ctx.db.query('users')
+    .withIndex('by_clerk_id',(q)=>q.eq('clerkId',args.clerkId))//if clerkId exist
+    .first()
+
+    if (existingUser) return;
+
+
+    //create user in db
+    await ctx.db.insert('users',{
+        username:args.username,
+        fullname:args.fullname,
+        email:args.email,
+        bio:args.bio,
+        image:args.image,
+        clerkId:args.clerkId,
+        followers:0,
+        following:0,
+        posts:0
+    })
+  }
+});
