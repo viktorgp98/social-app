8b94a8b2773ffd6573275295f404f1ac7d8115e5
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-28 19:38:55 -0400

    Clerk and Convex Provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,16 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { Slot } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import "./globals.css";
 
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }}>
           <Slot />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+    </ClerkAndConvexProvider>
   );
 }
