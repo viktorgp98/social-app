f9922acfbb7853bf6aad7a8223be08dfc47f02d7
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-05-30 19:28:16 -0400

    create function

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import { useUser } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
 
-const Create = () => {
+const CreateScreen = () => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [caption, setCaption] = useState("");
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [isSharing, setIsSharing] = useState(false);
+
   return (
     <View>
       <Text>Create</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Create
+export default CreateScreen;
