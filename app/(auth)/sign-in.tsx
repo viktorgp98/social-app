ee2aa56094e3b8f67e9a511143e1a4b9849c0942
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-06-04 19:01:50 -0400

    Keyboard manipulation in inputs

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,6 +6,8 @@ import React, { useRef } from "react";
 import {
   Image,
   Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   Text,
   TextInput,
   TouchableOpacity,
@@ -68,77 +70,84 @@ export default function Page() {
   };
 
   return (
-    <View style={styles.container} className="p-2">
-      {/* Brand section*/}
-      <View style={styles.brandSection}>
-        <Image
-          source={require("@/assets/images/auth-bg.png")}
-          style={styles.illustration}
-          resizeMode="contain"
-        />
-        <Text style={styles.appName}>HackerStuff</Text>
-        <Text style={styles.tagline}>Your daily dose of tech news</Text>
-      </View>
-      {/* LOGIN */}
-      <View style={styles.loginSection} className="mt-5">
-        <View
-          style={{
-            borderBottomWidth: 2,
-            borderColor: error ? COLORS.error : COLORS.primary,
-          }}
-          className="w-60 mb-5"
-        >
-          <TextInput
-            autoCapitalize="none"
-            inputMode="email"
-            value={emailAddress}
-            placeholder="Enter email"
-            placeholderTextColor={"white"}
-            onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
-            className="text-center text-white"
-            returnKeyType="next"
-            onSubmitEditing={focusOnPassword}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <View className="p-2">
+        {/* Brand section*/}
+        <View style={styles.brandSection}>
+          <Image
+            source={require("@/assets/images/auth-bg.png")}
+            style={styles.illustration}
+            resizeMode="contain"
           />
+          <Text style={styles.appName}>HackerStuff</Text>
+          <Text style={styles.tagline}>Your daily dose of tech news</Text>
         </View>
-        <View
-          style={{
-            borderBottomWidth: 2,
-            borderColor: error ? COLORS.error : COLORS.primary,
-          }}
-          className="mb-8 w-60"
-        >
-          <TextInput
-            value={password}
-            placeholder="Enter password"
-            placeholderTextColor={"white"}
-            secureTextEntry={true}
-            onChangeText={(password) => setPassword(password)}
-            className="text-center text-white"
-            ref={refPasswordInput}
-            returnKeyType="done"
-            onSubmitEditing={hideKeyboard}
-          />
-        </View>
-        {/* Error message */}
-        {error && <Text className="text-red-500 mb-5 font-bold">{error}</Text>}
-        <TouchableOpacity
-          onPress={onSignInPress}
-          className="rounded-xl  bg-primary w-60 px-20 py-2 mx-auto mb-5 shadow-lg shadow-primary"
-        >
-          <Text className="text-center text-white font-bold uppercase">
-            Login
-          </Text>
-        </TouchableOpacity>
-        <View
-          style={{ display: "flex", flexDirection: "row", gap: 5 }}
-          className="mt-5 "
-        >
-          <Text className="text-white">Don't have an account yet?</Text>
-          <Link href="/sign-up">
-            <Text className="font-bold text-md text-white">Sign Up</Text>
-          </Link>
+        {/* LOGIN */}
+        <View style={styles.loginSection} className="mt-5">
+          <View
+            style={{
+              borderBottomWidth: 2,
+              borderColor: error ? COLORS.error : COLORS.primary,
+            }}
+            className="w-60 mb-5"
+          >
+            <TextInput
+              autoCapitalize="none"
+              inputMode="email"
+              value={emailAddress}
+              placeholder="Enter email"
+              placeholderTextColor={"white"}
+              onChangeText={(emailAddress) => setEmailAddress(emailAddress)}
+              className="text-center text-white"
+              returnKeyType="next"
+              onSubmitEditing={focusOnPassword}
+            />
+          </View>
+          <View
+            style={{
+              borderBottomWidth: 2,
+              borderColor: error ? COLORS.error : COLORS.primary,
+            }}
+            className="mb-8 w-60"
+          >
+            <TextInput
+              value={password}
+              placeholder="Enter password"
+              placeholderTextColor={"white"}
+              secureTextEntry={true}
+              onChangeText={(password) => setPassword(password)}
+              className="text-center text-white"
+              ref={refPasswordInput}
+              returnKeyType="done"
+              onSubmitEditing={hideKeyboard}
+            />
+          </View>
+          {/* Error message */}
+          {error && (
+            <Text className="text-red-500 mb-5 font-bold">{error}</Text>
+          )}
+          <TouchableOpacity
+            onPress={onSignInPress}
+            className="rounded-xl  bg-primary w-60 px-20 py-2 mx-auto mb-5 shadow-lg shadow-primary"
+          >
+            <Text className="text-center text-white font-bold uppercase">
+              Login
+            </Text>
+          </TouchableOpacity>
+          <View
+            style={{ display: "flex", flexDirection: "row", gap: 5 }}
+            className="mt-5 "
+          >
+            <Text className="text-white">Don't have an account yet?</Text>
+            <Link href="/sign-up">
+              <Text className="font-bold text-md text-white">Sign Up</Text>
+            </Link>
+          </View>
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
