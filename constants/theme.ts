51fce6854ac38d945bcca7ba3721e7d1127b380b
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-06-03 20:00:25 -0400

    Theme correction

--- constants/theme.ts
+++ constants/theme.ts
@@ -1,8 +1,8 @@
 export const COLORS = {
   primary: "#4ADE80",
-  error:'#ef4444',
+  error: "#ef4444",
   secondary: "#2DD4BF",
-  background: "000000",
+  background: "#000000",
   surface: "#IAIAIA",
   surfaceLight: "#2A2A2A",
   white: "#FFFFFF",
