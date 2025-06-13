66f962a6767a53ee22e2ff7e040967c35e9860e8
viktorgp98 <viktor.gonzalezp98@gmail.com>
2025-06-13 19:22:50 -0400

    Creation of mock data

---
+++ constants/mock-data.ts
@@ -0,0 +1,37 @@
+export const STORIES = [
+  {
+    id: "1",
+    username: "You",
+    avatar:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?w=400&h=400&fit=crop",
+    hasStory: false,
+  },
+  {
+    id: "2",
+    username: "sarah.k",
+    avatar:
+      "https://images.unsplash.com/photo-1534528741775-53994a69daeb?w=400&h400&fit=crop",
+    hasStory: true,
+  },
+  {
+    id: "3",
+    username: "alex.m",
+    avatar:
+      "https://images.unsptash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit-crop",
+    hasStory: true,
+  },
+  {
+    id: "4",
+    username: "c.m",
+    avatar:
+      "https://images.unsptash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit-crop",
+    hasStory: true,
+  },
+  {
+    id: "5",
+    username: "aleeees",
+    avatar:
+      "https://images.unsptash.com/photo-1539571696357-5a69c17a67c6?w=400&h=400&fit-crop",
+    hasStory: true,
+  },
+];
